483fea2d6f0a1815ddd9a9cde57acfad98b5296a
grandmattress <aaranya.barman@mail.utoronto.ca>
2020-12-30 01:15:13 -0500

    started socket comms

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,18 @@
+var socket = io();
+
+
+let onDrop = (source, target, piece, newPos, oldPos, orn) => {
+    socket.emit("move", Chessboard.objToFen(newPos));
+}
+
+socket.on("change", (newPos) => {
+    board.position(newPos);
+});
+
 let config = {
     position: "start",
     draggable: true,
-    dropOffBoard: "snapback"
+    dropOffBoard: "snapback",
+    onDrop: onDrop
 }
-var board = Chessboard("board",config);
+var board = Chessboard("board", config);
--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+    console.log("A new user has connected.");
+    socket.on("move", newPos => {
+        console.log("A move was played");
+        io.emit("change",newPos); 
+    });
+});
+
+server.listen(PORT, () => {
     console.log("Server listening for connections on port", PORT);
 })
