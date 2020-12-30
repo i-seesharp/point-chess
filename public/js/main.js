10652cc11ae5b7615fd793424b7986c049eecd03
grandmattress <aaranya.barman@mail.utoronto.ca>
2020-12-30 03:23:42 -0500

    lobby and playing room functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,27 @@
 var socket = io();
+var board;
 
-
-let onDrop = (source, target, piece, newPos, oldPos, orn) => {
+var onDrop = (source, target, piece, newPos, oldPos, orn) => {
     socket.emit("move", Chessboard.objToFen(newPos));
 }
 
 socket.on("change", (newPos) => {
     board.position(newPos);
 });
+socket.on("play", (room, id) => {
+    console.log("In room ", room);
+    let config = {
+        position: "start",
+        draggable: true,
+        dropOffBoard: "snapback",
+        onDrop: onDrop
+    }
+    if(id === socket.id){
+        config.orientation = "white";
+    }
+    else{
+        config.orientation = "black";
+    }
+    board = Chessboard("board", config);
+});
 
-let config = {
-    position: "start",
-    draggable: true,
-    dropOffBoard: "snapback",
-    onDrop: onDrop
-}
-var board = Chessboard("board", config);
--- server.js
+++ server.js
@@ -9,15 +9,42 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
+const playingRoom = "playing-room";
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", (socket) => {
     console.log("A new user has connected.");
+    socket.join("lobby-room");
+    var lobby = Array.from(io.sockets.adapter.rooms.get("lobby-room"));
+    
+    if(lobby.length % 2 == 0){
+        socket.leave("lobby-room");
+        lobby = Array.from(io.sockets.adapter.rooms.get("lobby-room"));
+        let socketId = lobby[Math.floor(Math.random()*lobby.length)];
+        let randomSocket = io.sockets.sockets.get(socketId);
+
+        randomSocket.leave("lobby-room");
+        
+        randomSocket.join(playingRoom);
+        socket.join(playingRoom);
+
+        let randomNumber = Math.floor(Math.random()*2);
+        
+        if (randomNumber === 0){
+            io.to(playingRoom).emit("play", playingRoom, socket.id);
+        }
+        else{
+            io.to(playingRoom).emit("play", playingRoom, randomSocket.id);
+        }  
+    }
+    
     socket.on("move", newPos => {
         console.log("A move was played");
         io.emit("change",newPos); 
     });
+    
+    
 });
 
 server.listen(PORT, () => {
